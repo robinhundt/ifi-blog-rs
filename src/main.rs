efce04b6faf063533455ed155dd58d99fe2f590a
robinwilliam.hundt <robinwilliam.hundt@stud.uni-goettingen.de>
2019-10-06 16:42:24 +0200

    Forgot init call to logger

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), ifi_blog_rs::BotError> {
-    env_logger::from_env(Env::default().default_filter_or("info"));
+    env_logger::from_env(Env::default().default_filter_or("info")).init();
     let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
     let mut bot = BlogBot::new(
         token,
